5bdbebff00866f6d98783370d273c452b4846c24
JyotiPandey <pandey.in23@gmail.com>
2025-06-26 01:37:19 +0530

    set Default Index page

--- HavenClub/Program.cs
+++ HavenClub/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Membership}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
