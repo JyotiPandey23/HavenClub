a220a5931b8789e06c9ee85140a1116b1926b23b
JyotiPandey <pandey.in23@gmail.com>
2025-07-03 00:04:55 +0530

    memberlogin and pop show

--- HavenClub/Controllers/HomeController.cs
+++ HavenClub/Controllers/HomeController.cs
@@ -67,5 +67,10 @@ namespace HavenClub.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult MemberLogin()
+        {
+            return View();
+        }
     }
 }
